b0166b052b18191738a90a4b7532c9b882d096b0
Alejandro Guinot <al375927@uji.es>
2025-01-23 08:45:56 +0100

    Fix update pairing when incrementing points for the correct player

--- app/Http/Controllers/PairingController.php
+++ app/Http/Controllers/PairingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pairing;
+use App\Models\Registration;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -16,30 +17,42 @@ class PairingController extends Controller
         $validated = $request->validate([
             'matchResult' => 'required|' . Rule::in(['P1', 'P2', 'TIE']),
         ]);
-        
-        
         if ($validated['matchResult'] == 'P1') {
-            $winner = $pairing->player1_id;
             $pairing->update([
-                'winner_id' => $winner,
+                'winner_id' => $pairing->player1_id,
             ]);
-            $pairing->winner->increment('points', 3);
+            $reg = Registration::where('tournament_id', $pairing->tournament_id)
+                                ->where('player_id', $pairing->player1_id)
+                                ->get()
+                                ->first();
+            $reg->increment('points', 3);
         }
         
         if ($validated['matchResult'] == 'P2') {
-            $winner = $pairing->player2_id;
             $pairing->update([
-                'winner_id' => $winner,
+                'winner_id' => $pairing->player2_id,
             ]);
-            $pairing->winner->increment('points', 3);
+            $reg = Registration::where('tournament_id', $pairing->tournament_id)
+                                ->where('player_id', $pairing->player2_id)
+                                ->get()
+                                ->first();
+            $reg->increment('points', 3);
         }
         
         if ($validated['matchResult'] == 'TIE') {
             $pairing->update([
                 'is_a_tie' => true,
             ]);
-            $pairing->player1->increment('points', 1);
-            $pairing->player2->increment('points', 1);
+            $reg1 = Registration::where('tournament_id', $pairing->tournament_id)
+                                ->where('player_id', $pairing->player1_id)
+                                ->get()
+                                ->first();
+            $reg2 = Registration::where('tournament_id', $pairing->tournament_id)
+                                ->where('player_id', $pairing->player2_id)
+                                ->get()
+                                ->first();
+            $reg1->increment('points', 1);
+            $reg2->increment('points', 1);
         }
         
         return redirect(route('tournaments.show', $pairing->tournament_id));
